63b4c0499e636cc2fa9914bd988ab74f57f6da41
sarahduany22y <sarahduany22y@gmail.com>
2026-09-18 17:39:32 -0300

    Update links-amador.js

--- links-amador.js
+++ links-amador.js
@@ -1,6 +1,5 @@
 // links-amador.js
 window.linksAmador = [
   "https://drive.google.com/file/d/1mTMSC93PNDBsDtxcUJT5OIJi_FXzQYYo/preview",
-  "https://drive.google.com/file/d/1QvOlArDxLgFDR4PDsDzNF6Brd_PpnhJH/preview",
-  "https://www.redgifs.com/ifr/weirdaltruisticdaddylonglegs"
+  "https://drive.google.com/file/d/1QvOlArDxLgFDR4PDsDzNF6Brd_PpnhJH/preview"
 ];
