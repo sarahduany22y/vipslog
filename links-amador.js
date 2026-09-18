a344d7a811a4368afaa52469a6c56b500df29f33
sarahduany22y <sarahduany22y@gmail.com>
2026-09-18 04:04:11 -0300

    Update links-amador.js

--- links-amador.js
+++ links-amador.js
@@ -1,5 +1,5 @@
 window.linksAmador = [
-  "https://www.exemplo.com/embed/video1.mp4",
-  "https://www.exemplo.com/embed/video2",
+  "https://drive.google.com/file/d/1mTMSC93PNDBsDtxcUJT5OIJi_FXzQYYo/view?usp=sharing",
+  "https://drive.google.com/file/d/1QvOlArDxLgFDR4PDsDzNF6Brd_PpnhJH/view?usp=sharing",
   "https://www.exemplo.com/embed/video3"
 ];
