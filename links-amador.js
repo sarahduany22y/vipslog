47394fabaddf6685b4666337f12c28dd37eedd20
sarahduany22y <sarahduany22y@gmail.com>
2026-09-18 20:26:49 -0300

    Add new video links to linksAmador array

--- links-amador.js
+++ links-amador.js
@@ -2,5 +2,6 @@
 window.linksAmador = [
   "https://drive.google.com/file/d/1mTMSC93PNDBsDtxcUJT5OIJi_FXzQYYo/preview",
   "https://drive.google.com/file/d/1QvOlArDxLgFDR4PDsDzNF6Brd_PpnhJH/preview",
-  "https://iframe.mediadelivery.net/embed/756775/8ae4f023-c6a1-4411-9b93-167664981a81"
+  "https://iframe.mediadelivery.net/embed/756775/8ae4f023-c6a1-4411-9b93-167664981a81",
+  "https://videos.boafoda.com/player/index/345881/1038/13788"
 ];
