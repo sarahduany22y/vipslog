8abd5e17f22ae98d0d42d7725afda04df3bf578c
sarahduany22y <sarahduany22y@gmail.com>
2026-09-18 17:36:23 -0300

    Replace RedGIFs embed with direct link

--- links-amador.js
+++ links-amador.js
@@ -2,5 +2,5 @@
 window.linksAmador = [
   "https://drive.google.com/file/d/1mTMSC93PNDBsDtxcUJT5OIJi_FXzQYYo/preview",
   "https://drive.google.com/file/d/1QvOlArDxLgFDR4PDsDzNF6Brd_PpnhJH/preview",
-  "<div style='position:relative; padding-bottom:177.78%'><iframe src='https://www.redgifs.com/ifr/weirdaltruisticdaddylonglegs' frameBorder='0' scrolling='no' width='100%' height='100%' style='position:absolute; top:0; left:0;' allowFullScreen></iframe></div><p><a href='https://www.redgifs.com/watch/weirdaltruisticdaddylonglegs'>via RedGIFs</a></p>"
+  "https://www.redgifs.com/ifr/weirdaltruisticdaddylonglegs"
 ];
