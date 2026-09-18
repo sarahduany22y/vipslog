79b8156fc3f15e73d558cbde4237bc0221896fa9
sarahduany22y <sarahduany22y@gmail.com>
2026-09-18 04:23:32 -0300

    Update links-amador.js

--- links-amador.js
+++ links-amador.js
@@ -1,5 +1,5 @@
+// links-amador.js
 window.linksAmador = [
   "https://drive.google.com/file/d/1mTMSC93PNDBsDtxcUJT5OIJi_FXzQYYo/preview",
-  "https://drive.google.com/file/d/1QvOlArDxLgFDR4PDsDzNF6Brd_PpnhJH/preview",
-  "https://www.exemplo.com/embed/video3"
+  "https://drive.google.com/file/d/1QvOlArDxLgFDR4PDsDzNF6Brd_PpnhJH/preview"
 ];
