4d0a4730f2702b73af004e4bf298f8074296d9d0
sarahduany22y <sarahduany22y@gmail.com>
2026-09-18 17:34:21 -0300

    Add new links to linksAmador array

--- links-amador.js
+++ links-amador.js
@@ -1,5 +1,6 @@
 // links-amador.js
 window.linksAmador = [
   "https://drive.google.com/file/d/1mTMSC93PNDBsDtxcUJT5OIJi_FXzQYYo/preview",
-  "https://drive.google.com/file/d/1QvOlArDxLgFDR4PDsDzNF6Brd_PpnhJH/preview"
+  "https://drive.google.com/file/d/1QvOlArDxLgFDR4PDsDzNF6Brd_PpnhJH/preview",
+  "<div style='position:relative; padding-bottom:177.78%'><iframe src='https://www.redgifs.com/ifr/weirdaltruisticdaddylonglegs' frameBorder='0' scrolling='no' width='100%' height='100%' style='position:absolute; top:0; left:0;' allowFullScreen></iframe></div><p><a href='https://www.redgifs.com/watch/weirdaltruisticdaddylonglegs'>via RedGIFs</a></p>"
 ];
